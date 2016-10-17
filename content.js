e61a2dd23fb0bb5d71da38baa1b6b2aa464a2c8e
Robert <robert@engagio.com>
2016-10-17 13:23:43 -0700

    refactors send message function

--- content.js
+++ content.js
@@ -1,23 +1,27 @@
+var sendMessage = function(message) {
+  chrome.runtime.sendMessage(message);
+}
+
 Mousetrap.bind('alt+shift+[', function(e) {
   console.log('[content] pressed alt+shift+[: ', e);
-  chrome.runtime.sendMessage({
+  sendMessage({
     type: 'MOVE_TAB',
     direction: -1
   });
 });
 Mousetrap.bind('alt+shift+]', function(e) {
-  chrome.runtime.sendMessage({
+  sendMessage({
     type: 'MOVE_TAB',
     direction: 1
   });
 });
 Mousetrap.bind('alt+d', function(e) {
-  chrome.runtime.sendMessage({
+  sendMessage({
     type: 'DUPLICATE_TAB'
   });
 });
 Mousetrap.bind('alt+w', function(e) {
-  chrome.runtime.sendMessage({
+  sendMessage({
     type: 'CLOSE_TABS_TO_RIGHT',
     direction: 1
   });
