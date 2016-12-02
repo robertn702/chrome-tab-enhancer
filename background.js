257602db2c1b738de5f799bce45b8ee5e8f38d6f
Robert <robert@engagio.com>
2016-12-02 09:24:46 -0800

    fixes removing tabs to right

--- background.js
+++ background.js
@@ -1,3 +1,11 @@
+var tabPosition = function(allWindowTabs, tab) {
+  var tabLen = allWindowTabs.length;
+  return {
+    isLast: allWindowTabs.length === tab.index + 1,
+    isFirst: tab.index === 0
+  }
+}
+
 var tabUtils = {
   allWindowTabs: function(cb) {
     chrome.tabs.query({currentWindow: true}, cb);
@@ -15,9 +23,10 @@ var tabUtils = {
 chrome.runtime.onMessage.addListener(function(message, sender) {
   switch (message.type) {
     case 'MOVE_TAB':
+      console.log('[background] MOVE_TAB');
       if (sender.tab && message.direction || message.index != null) {
         console.log('[background] message.index: ', message.index);
-        chrome.tabs.query({currentWindow: true}, function(allWindowTabs) {
+        tabUtils.allWindowTabs(function(allWindowTabs) {
           var lastIndex = allWindowTabs.length - 1;
           var newIndex = (message.index != null)
             ? message.index
@@ -30,13 +39,15 @@ chrome.runtime.onMessage.addListener(function(message, sender) {
       }
       break;
     case 'DUPLICATE_TAB':
+      console.log('[background] DUPLICATE_TAB');
       if (sender.tab) {
         chrome.tabs.duplicate(sender.tab.id);
       }
       break;
     case 'CLOSE_TABS_TO_RIGHT':
+      console.log('[background] CLOSE_TABS_TO_RIGHT');
       if (sender.tab) {
-        chrome.tabs.query({currentWindow: true}, function(allWindowTabs) {
+        tabUtils.allWindowTabs(function(allWindowTabs) {
           if (tabPosition(allWindowTabs, sender.tab).isLast) {
             return;
           }
@@ -50,8 +61,9 @@ chrome.runtime.onMessage.addListener(function(message, sender) {
       }
       break;
     case 'RELOAD_TABS':
+      console.log('[background] RELOAD_TABS');
       if (sender.tab) {
-        chrome.tabs.query({currentWindow: true}, function(allWindowTabs) {
+        tabUtils.allWindowTabs(function(allWindowTabs) {
           allWindowTabs.forEach(function(tab) {
             chrome.tabs.reload(tab.id);
           });
@@ -59,6 +71,7 @@ chrome.runtime.onMessage.addListener(function(message, sender) {
       }
       break;
     case 'HIGHLIGHT_TABS':
+      console.log('[background] HIGHLIGHT_TABS');
       if (sender.tab) {
         tabUtils.allWindowTabs(function(allWindowTabs) {
           tabUtils.allHighlightedTabs(function(allHighlightedTabs) {
@@ -89,26 +102,34 @@ chrome.runtime.onMessage.addListener(function(message, sender) {
       }
       break;
     case 'MOVE_TO_NEW_WINDOW':
+      console.log('[background] MOVE_TO_NEW_WINDOW');
       if (sender.tab) {
-        tabUtils.allHighlightedTabs(function(allHighlightedTabs) {
-          chrome.windows.create(function(newWindow) {
-          console.log('[background] newWindow: ', newWindow);
-            var highlightedTabIds = allHighlightedTabs.map(function(highlightedTab) {
-              return highlightedTab.id;
-            });
-            console.log('[background] highlightedTabIds: ', highlightedTabIds);
-            chrome.tabs.move(highlightedTabIds, {
-              windowId: newWindow.id,
-              index: -1
-            }, function() {
-              tabUtils.getCurrent(function(currentTab) {
-                console.log('[background] currentTab: ', currentTab);
-                chrome.tabs.remove(currentTab.id);
+        tabUtils.allWindowTabs(function(allWindowTabs) {
+          /* don't create new window if current window has only one tab */
+          if (allWindowTabs.length === 1) {
+            return;
+          }
+          tabUtils.allHighlightedTabs(function(allHighlightedTabs) {
+            chrome.windows.create(function(newWindow) {
+            console.log('[background] newWindow: ', newWindow);
+              var highlightedTabIds = allHighlightedTabs.map(function(highlightedTab) {
+                return highlightedTab.id;
+              });
+              console.log('[background] highlightedTabIds: ', highlightedTabIds);
+              chrome.tabs.move(highlightedTabIds, {
+                windowId: newWindow.id,
+                index: -1
+              }, function() {
+                tabUtils.getCurrent(function(currentTab) {
+                  console.log('[background] currentTab: ', currentTab);
+                  chrome.tabs.remove(currentTab.id);
+                });
               });
             });
           });
-        });
+        })
       }
+      break;
     default:
       return;
   }
